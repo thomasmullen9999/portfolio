60fa1ffb34a4e02c036206e9b6d245d14f496e31
thomasmullen9999 <thomasmullen9999@gmail.com>
2024-05-07 22:28:42 +0100

    Add logos to skills page

--- portfolio/components/Skills.tsx
+++ portfolio/components/Skills.tsx
@@ -4,12 +4,28 @@ const Skills = () => {
   return (
     <div id="skills">
       <h2>Skills</h2>
-      <h3>Languages: Python, Javascript (using Node.js and browsers as runtime environment), Typescript</h3>
+      <h3>Languages: C, Python, Javascript (using Node.js and browsers as runtime environment), Typescript</h3>
       <h3>Testing: Test Driven Development (TDD), Jest, Supertest</h3>
       <h3>Back-end: SQL, PostgreSQL, SQLite, Express, database seeding, migrations</h3>
       <h3>Front-end: HTML, CSS, React, React Native, Lighthouse, other accessibility tools</h3>
       <h3>Hosting: Netlify, Cloudflare, GitHub Pages</h3>
       <h3>Soft skills: Technical communication, paired programming, SDL, agile and SCRUM Methodologies</h3>
+
+      <img src="../images/html5.png" className="icon"></img>
+      <img src="../images/css.png" className="icon"></img>
+      <img src="../images/javascript.png" className="icon"></img>
+      <img src="../images/typescript.png" className="icon"></img>
+      <img src="../images/sql.png" className="icon"></img>
+      <img src="../images/sqlite.png" className="icon"></img>
+      <img src="../images/postgres.png" className="icon"></img>
+      <img src="../images/python.png" className="icon"></img>
+      <img src="../images/flask.png" className="icon"></img>
+      <img src="../images/node.png" className="icon"></img>
+      <img src="../images/react.png" className="icon"></img>
+      <img src="../images/jest.png" className="icon"></img>
+      <img src="../images/netlify.png" className="icon"></img>
+      <img src="../images/github.png" className="icon"></img>
+      <img src="../images/c.png" className="icon"></img>
 
       <h2>Certifications</h2>
 
